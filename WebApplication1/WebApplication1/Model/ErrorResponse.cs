43094a2a5ba95d5fa2768924c5fddd48507e49ab
kozurap <timur.122@yandex.ru>
2023-10-17 18:43:02 +0300

    fix

---
+++ WebApplication1/WebApplication1/Model/ErrorResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Model
+{
+    public class ErrorResponse
+    {
+        public string? Message { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Model/ValidationErrorResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Model
+{
+    public class ValidationErrorResponse : ErrorResponse
+    {
+        (string, string) Errors { get; set; }
+    }
+}
